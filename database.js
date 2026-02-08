23b95728f45a79dda24b8374de7fb8be6fb3f22b
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2026-02-08 22:47:34 +0530

    Did some corrections in database connections

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect(`${process.env.MONGO_URI}`).then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to database successfully");
-});
+}).catch(err => console.log(err));
 
